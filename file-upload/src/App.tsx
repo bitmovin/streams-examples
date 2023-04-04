c0517b6fb44b85b6201a547b2f264e937ddb25cc
Hannes Linder <hannes.linder@bitmovin.com>
2023-04-04 09:57:43 +0200

    Add file upload example

---
+++ file-upload/src/App.tsx
@@ -0,0 +1,16 @@
+import { FileUpload } from "./FileUpload";
+import "./App.css";
+
+// Replace with your Bitmovin API key
+const API_KEY = "YOUR_API_KEY";
+
+function App() {
+	return (
+		<div className="App">
+			<h2>Streams file upload</h2>
+			<FileUpload apiKey={API_KEY} />
+		</div>
+	);
+}
+
+export default App;
